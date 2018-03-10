651e911f261501e1330cabc9fe8a868a7267df0c
AFM92 <a.farhad92@gmail.com>
2018-03-10 12:54:40 +0300

    Calculate return sum inserting two int values

--- src/ru.afm/Calculate.java
+++ src/ru.afm/Calculate.java
@@ -2,6 +2,9 @@ package ru.afm;
 
 public class Calculate{
     public static void main(String[] args) {
+        int a = Integer.valueOf(args[0]);
+        int b = Integer.valueOf(args[1]);
         System.out.println("Calculating...");
+        System.out.println("Sum = " + (a+b));
     }
 }
